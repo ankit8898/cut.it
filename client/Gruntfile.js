756e9e887c412d6dd986af0dbdbda4a36b493b48
Ankit gupta <ankit.gupta8898@gmail.com>
2015-03-27 15:57:10 -0400

    client build

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: '../public'
+    dist: '../server/public'
   };
 
   // Define the configuration for all the tasks
